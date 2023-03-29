7de5770e69f4a993f47f342737426b3cd8cf2db0
Lukas Rauh <lukas.rauh@ipa.fraunhofer.de>
2023-03-29 10:01:32 +0200

    adds getEnv to load env correctly

--- src/components/StreamComponent.vue
+++ src/components/StreamComponent.vue
@@ -5,6 +5,9 @@
 </template>
   
   <script>
+
+  import getEnv from '@/utils/env'
+
   export default {
     data() {
       return {
@@ -13,8 +16,13 @@
     },
     methods: {
       async fetchStreamURL() {
-        const url = process.env.VUE_APP_AAS_SERVER_HOST +'/shells/'+process.env.VUE_APP_AAS_ID+'/aas/submodels/OperationalData/submodel/submodelElements/Endpoint/value'
-        const res = await fetch(url)
+        const url = getEnv('VUE_APP_AAS_SERVER_HOST') + '/shells/' + getEnv('VUE_APP_AAS_ID') + '/aas/submodels/OperationalData/submodel/submodelElements/Endpoint/value'
+        const res = await fetch(url).then(
+          (response) => {
+            console.log("successful recieved response from AAS server ", response)
+          }).catch(exception => {
+            console.error('stream url fetch failed: ' + exception)
+          });
         const data = await res.json()
         return data
       }
