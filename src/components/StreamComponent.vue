0c7c3fef389d2e38b5947bbfb748767697c9c6e9
Lukas Rauh <lukas.rauh@ipa.fraunhofer.de>
2023-04-03 14:28:07 +0200

    adds registry interaction to find server

--- src/components/StreamComponent.vue
+++ src/components/StreamComponent.vue
@@ -11,18 +11,36 @@
   export default {
     data() {
       return {
-        imgSrc: ''
+        imgSrc: '',
       }
     },
     methods: {
-      async fetchStreamURL() {
-        const url = getEnv('VUE_APP_AAS_SERVER_HOST') + '/shells/' + getEnv('VUE_APP_AAS_ID') + '/aas/submodels/OperationalData/submodel/submodelElements/Endpoint/value'
+
+      async discoverAasServer(){
+        
+        let url = getEnv('VUE_APP_AAS_REGISTRY_HOST') + '/api/v1/registry/' + getEnv('VUE_APP_AAS_ID')
+
         let res = await fetch(url).then(
           (response) => {
-            console.log("successful recieved response from AAS server ", response)
+            console.log("successful recieved response from AAS registry ", response)
             return response.json()
           }).catch(exception => {
-            console.error('stream url fetch failed: ' + exception)
+            console.error('AAS fetch from registry failed: ' + exception)
+            return null
+          });
+        return res
+        
+      },
+
+      async fetchStreamURL(baseUrl) {
+        // const url = getEnv('VUE_APP_AAS_SERVER_HOST') + '/shells/' + getEnv('VUE_APP_AAS_ID') + '/aas/submodels/OperationalData/submodel/submodelElements/Endpoint/value'
+        const url = baseUrl + '/values'
+        let res = await fetch(url).then(
+          (response) => {
+            console.log("successful recieved submodel from AAS server ", response)
+            return response.json()
+          }).catch(exception => {
+            console.error('submodel fetch failed: ' + exception)
             return null
           });
         return res
@@ -30,7 +48,27 @@
     },
     async created() {
       
-      this.imgSrc = await this.fetchStreamURL()
+      let registryResponse = await this.discoverAasServer()
+
+      if (registryResponse != null){
+
+        let operationSubmodel = registryResponse.submodels.filter((sm) => sm.idShort === "OperationalData")[0].endpoints[0].address
+
+        if (typeof operationSubmodel !== "undefined"){
+
+          let submodel = await this.fetchStreamURL(operationSubmodel)
+
+          if (submodel.hasOwnProperty('Endpoint')){
+            this.imgSrc = submodel.Endpoint
+          } else {
+            console.warn("fetched submodel has no property 'Endpoint': " + submodel);
+          }
+
+        }
+
+      }
+
+      
       
       console.log('StreamComponent')
       console.log(this.imgSrc)
