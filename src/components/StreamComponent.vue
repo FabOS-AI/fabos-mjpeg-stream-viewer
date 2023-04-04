bb82c7c2c45fdb418fce533f44f1b2590b3f03e1
Vincent <vincent.ofosu.nyako@ipa.fraunhofer.de>
2023-04-04 11:22:34 +0200

    adds registry request before aas server request

--- src/components/StreamComponent.vue
+++ src/components/StreamComponent.vue
@@ -16,16 +16,14 @@
     },
     methods: {
 
-      async discoverAasServer(){
+    async discoverAasServer() {
         
         let url = getEnv('VUE_APP_AAS_REGISTRY_HOST') + '/api/v1/registry/' + getEnv('VUE_APP_AAS_ID')
-
         let res = await fetch(url).then(
           (response) => {
-            console.log("successful recieved response from AAS registry ", response)
             return response.json()
           }).catch(exception => {
-            console.error('AAS fetch from registry failed: ' + exception)
+          console.error(`AAS fetch for ID '${getEnv('VUE_APP_AAS_ID')}' from registry '${getEnv('VUE_APP_AAS_REGISTRY_HOST')}' failed: \n` + exception)
             return null
           });
         return res
@@ -33,45 +31,65 @@
       },
 
       async fetchStreamURL(baseUrl) {
-        // const url = getEnv('VUE_APP_AAS_SERVER_HOST') + '/shells/' + getEnv('VUE_APP_AAS_ID') + '/aas/submodels/OperationalData/submodel/submodelElements/Endpoint/value'
+      
         const url = baseUrl + '/values'
         let res = await fetch(url).then(
           (response) => {
-            console.log("successful recieved submodel from AAS server ", response)
-            return response.json()
+          //console.log("successful recieved submodel from AAS server ", response)
+          let result = response.json()
+          console.log("found submodel at AAS server:", result)
+          return result
           }).catch(exception => {
             console.error('submodel fetch failed: ' + exception)
             return null
           });
         return res
-      }
     },
-    async created() {
+
+    doRefresh(){
+      this.getStreamUrl()
+    },
+
+    async getStreamUrl() {
+
+      this.imgSrc = null
       
       let registryResponse = await this.discoverAasServer()
 
-      if (registryResponse != null){
+      if (registryResponse != null) {
 
-        let operationSubmodel = registryResponse.submodels.filter((sm) => sm.idShort === "OperationalData")[0].endpoints[0].address
+        console.log("found AAS at registry:", registryResponse)
+        let operationSubmodel = registryResponse.submodels.filter((sm) => sm.idShort === "OperationalData")
 
-        if (typeof operationSubmodel !== "undefined"){
+        if (typeof operationSubmodel !== "undefined") {
 
-          let submodel = await this.fetchStreamURL(operationSubmodel)
+          let aasUrl = operationSubmodel[0].endpoints[0].address
+          if (typeof aasUrl !== "undefined" && aasUrl.length > 0) {
+            let submodel = await this.fetchStreamURL(aasUrl)
 
-          if (submodel.hasOwnProperty('Endpoint')){
+            if (Object.prototype.hasOwnProperty.call(submodel, 'Endpoint')) {
+
+              console.log("found matching submodel:", submodel)
             this.imgSrc = submodel.Endpoint
+              console.log(`loaded stream URL: '${this.imgSrc}'`)
           } else {
             console.warn("fetched submodel has no property 'Endpoint': " + submodel);
           }
-
+          } else {
+            console.warn("fetched submodel has no endpoint to access");
         }
-
+        } else {
+          console.warn("did not find submodel 'OperationalData' in AAS from registry");
       }
-
+      } else {
+        console.warn("did not receive any submodels from AAS registry for AAS with id: " + getEnv('VUE_APP_AAS_ID') + " at: " + getEnv('VUE_APP_AAS_REGISTRY_HOST'));
+      }
+    }
+  },
+  async created() {
       
+    this.getStreamUrl()
       
-      console.log('StreamComponent')
-      console.log(this.imgSrc)
     },
   }
   </script>
